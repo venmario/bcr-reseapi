a8f5edcd5233bf877e8315a13f5cda3a5ebda164
venmario <glacius2ndmioogg@gmail.com>
2023-12-22 00:03:37 +0700

    feat:change knexfile config

--- knexfile.ts
+++ knexfile.ts
@@ -5,37 +5,24 @@ import "dotenv/config";
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: "postgresql",
-    connection: {
-      host: "localhost",
-      port: 5432,
-      user: "postgres",
-      password: "root",
-      database: "bcr",
-    },
+    connection: process.env.DATABASE_URL,
     seeds: {
-      directory: "./database/seeds/dev",
+      directory: "./database/seeds/dev"
     },
     migrations: {
-      directory: "./database/migrations",
-    },
+      directory: "./database/migrations"
+    }
   },
   production: {
     client: "pg",
-    // connection: {
-    //   host: "fdaa:3:d679:0:1::3",
-    //   port: 5432,
-    //   user: "postgres",
-    //   password: "0T9Riw7KwtHxKIR",
-    //   database: "bcr_backend_mario",
-    // },
     connection: process.env.DATABASE_URL,
     seeds: {
-      directory: "./database/seeds/dev",
+      directory: "./database/seeds/dev"
     },
     migrations: {
-      directory: "./database/migrations",
-    },
-  },
+      directory: "./database/migrations"
+    }
+  }
 };
 
 module.exports = config;
